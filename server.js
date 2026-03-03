ce927243352d69f2fbc2947f423bb922c27ec264
Anil Kumar <ak24983152@gmail.com>
2026-03-03 21:14:51 +0530

    configured according to deployment purpose

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const OSNetwork = require('os').networkInterfaces()
-const ip = OSNetwork['Wi-Fi'][2].address
-const port = 3000
-const appIdentifier = process.env.APP_NAME
-app.use('/images', express.static(path.join(__dirname, 'images')))
+
+const port = process.env.PORT || 3000;        // dynamic port
+const appIdentifier = process.env.APP_NAME || 'app';
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
     console.log('Requested by: ' + appIdentifier);
 });
 
-
-app.listen(port, ip, () => {
-    console.log(`${appIdentifier} requested and Server is running on port: ${port} and ip address: ${ip}    `);
+// listen on 0.0.0.0 so Docker can reach it
+app.listen(port, '0.0.0.0', () => {
+    console.log(`${appIdentifier} running on port: ${port}`);
 });
